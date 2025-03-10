6c6ed1b39cde1d9c401e163517b52132f89da85c
JoaoMalko <163422055+JoaoMalko@users.noreply.github.com>
2025-03-10 09:22:30 -0300

    Diminuir Botão

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuaFonteBotao = document.getElementById('aumentar-fonte');
 let tamanhoAtualFonte = 1;
 aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte += 0.1;
-        document.boby.style.fontSize = ${tamanhoAtualFonte}
+        document.boby.style.fontSize = '${tamanhoAtualFonte}ren';
+
     });
-})
+
+    diminuaFonteBotao.addEventListener('click',function(){
+        tamanhoAtualFonte -= 0.1;
+        document.boby.style.fontSize = '${tamanhoAtualFonte}ren';
+});
+});
