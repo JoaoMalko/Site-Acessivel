9b29f2958676a21ea5329f2b2de89ee8b5a091af
JoaoMalko <163422055+JoaoMalko@users.noreply.github.com>
2025-03-24 20:19:57 -0300

    Script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
 const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 const diminuaFonteBotao = document.getElementById('aumentar-fonte');
 
+ botaoDeAcessibilidade.addEventListener('click', function){
+    
+
+
+    
 let tamanhoAtualFonte = 1;
 aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte += 0.1;
