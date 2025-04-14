5371a63a87d75d30c9692b3a39a0a87ee1b995ba
JoaoMalko <163422055+JoaoMalko@users.noreply.github.com>
2025-04-14 10:49:24 -0300

    script

--- script.js
+++ script.js
@@ -5,26 +5,30 @@ const diminuaFonteBotao = document.getElementById('opcoes-acessibilidade');
  botaoDeAcessibilidade.addEventListener('click', function){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     botaoDeAcessibilidade.classList.toggle('apresenta-lista');
+
  })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuaFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste');
 
 let tamanhoAtualFonte = 1;
+
+
 aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte += 0.1;
-        document.boby.style.fontSize = `${tamanhoAtualFonte}ren`;
+        document.boby.style.fontSize = `${tamanhoAtualFonte}rem`
 
     });
 
     diminuiFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte -= 0.1;
-        document.boby.style.fontSize = `${tamanhoAtualFonte}ren`;
+        document.boby.style.fontSize = `${tamanhoAtualFonte}rem`
 
       })
         alternaContraste.addEventListener('click',function(){
             document.body.classList.toggle('alto-contraste')
-      });
+      })
 
-});
+})
